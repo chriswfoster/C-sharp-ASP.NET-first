60753e13fcd013352c770c0c5f195dc01547ace4
Chris <chriswfoster@gmail.com>
2018-06-24 23:51:30 -0500

    Basic prodel for products built

--- ProductsApp/Models/Product.cs
+++ ProductsApp/Models/Product.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ProductsApp.Models
+namespace ProductsApp.Models /// a model is an object that represents the data in your application.
+// ASP.NET web api can automatically serialize your model to JSON, xml, or some other format.
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
     }
 }
